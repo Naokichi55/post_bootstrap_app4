22f47f0dd496a400a7266f8a19c4adbb39ba1f56
Naokichi55 <ibosanganba@gmail.com>
2023-08-11 16:46:55 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/deestroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+  resources :posts
 end
